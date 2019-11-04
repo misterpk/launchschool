57063d730aa50aaef2ae89e0e21ddfbe9dea5611
Nick Kim <nick.kim@gmail.com>
2019-11-03 21:57:34 -0800

    bonus 1 and 3 for rps

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -1,13 +1,24 @@
-VALID_CHOICES = %w(rock paper scissors)
+# TODO: Bonus 2: Typing the full word "rock" or "lizard" is tiring. Update the
+# program so the user can type "r" for "rock", "p" for "paper", etc. Note that
+# if you do bonus #1, you'll have two words that start with "s". How do you
+# resolve that?
+
+VALID_CHOICES = %w(rock paper scissors spock lizard)
+WINNING_CHOICES = {
+  rock: %w(lizard scissors),
+  paper: %w(rock spock),
+  scissors: %w(paper lizard),
+  spock: %w(scissors rock),
+  lizard: %w(spock paper)
+}
+WINNING_SCORE = 5
 
 def prompt(message)
   puts("=> #{message}")
 end
 
 def win?(first, second)
-  (first == 'rock' && second == 'scissors') ||
-    (first == 'paper' && second == 'rock') ||
-    (first == 'scissors' && second == 'paper')
+  WINNING_CHOICES[first.to_sym].include?(second)
 end
 
 def display_results(player, computer)
@@ -19,6 +30,19 @@ def display_results(player, computer)
     prompt("It's a tie!")
   end
 end
+
+def update_score(score, player, computer)
+  score[:player] += 1 if win?(player, computer)
+  score[:computer] += 1 if win?(computer, player)
+end
+
+score = {
+  player: 0,
+  computer: 0
+}
+
+prompt('Let\'s play rock, paper, scissors, spock, lizard. '\
+       'The first person to 5 wins!')
 
 loop do
   choice = ''
@@ -38,10 +62,16 @@ loop do
   prompt("You chose: #{choice}; Computer chose: #{computer_choice}")
 
   display_results(choice, computer_choice)
+  update_score(score, choice, computer_choice)
 
-  prompt("Do you want to play again?")
-  answer = gets.chomp
-  break unless answer.downcase.start_with?('y')
+  prompt("Your score: #{score[:player]} : Computer score: #{score[:computer]}")
+  if score[:player] == WINNING_SCORE
+    prompt("You win!")
+    break
+  elsif score[:computer] == WINNING_SCORE
+    prompt("The computer wins!")
+    break
+  end
 end
 
 prompt("Thank you for playing. Good bye!")
