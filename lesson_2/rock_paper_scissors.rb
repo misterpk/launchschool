d5c7a0917be8013fb19f3504f74bd3cee3f12ed0
Nick Kim <nick.kim@gmail.com>
2019-11-04 18:48:48 -0800

    bonus 2 for rps

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -1,9 +1,3 @@
-# TODO: Bonus 2: Typing the full word "rock" or "lizard" is tiring. Update the
-# program so the user can type "r" for "rock", "p" for "paper", etc. Note that
-# if you do bonus #1, you'll have two words that start with "s". How do you
-# resolve that?
-
-VALID_CHOICES = %w(rock paper scissors spock lizard)
 WINNING_CHOICES = {
   rock: %w(lizard scissors),
   paper: %w(rock spock),
@@ -12,6 +6,13 @@ WINNING_CHOICES = {
   lizard: %w(spock paper)
 }
 WINNING_SCORE = 5
+MOVES_KEY = {
+  rock: 'r',
+  paper: 'p',
+  scissors: 's',
+  spock: 'k',
+  lizard: 'l'
+}
 
 def prompt(message)
   puts("=> #{message}")
@@ -36,6 +37,21 @@ def update_score(score, player, computer)
   score[:computer] += 1 if win?(computer, player)
 end
 
+def generate_player_choice
+  choice = ''
+  loop do
+    prompt("Choose one: #{MOVES_KEY.values.join(', ')}")
+    choice = gets.chomp
+
+    if MOVES_KEY.value?(choice)
+      break
+    else
+      prompt("That's not a valid choice.")
+    end
+  end
+  choice
+end
+
 score = {
   player: 0,
   computer: 0
@@ -44,27 +60,31 @@ score = {
 prompt('Let\'s play rock, paper, scissors, spock, lizard. '\
        'The first person to 5 wins!')
 
+legend = <<-LEGEND
+Key:
+     Rock: #{MOVES_KEY[:rock]}
+     Paper: #{MOVES_KEY[:paper]}
+     Scissors: #{MOVES_KEY[:scissors]}
+     Spock: #{MOVES_KEY[:spock]}
+     Lizard: #{MOVES_KEY[:lizard]}
+LEGEND
+
+prompt(legend)
+
 loop do
-  choice = ''
-  loop do
-    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-    choice = gets.chomp
+  player_choice = generate_player_choice
+  computer_choice = MOVES_KEY.values.sample
 
-    if VALID_CHOICES.include?(choice)
-      break
-    else
-      prompt("That's not a valid player.")
-    end
-  end
+  player_key = MOVES_KEY.key(player_choice).to_s
+  computer_key = MOVES_KEY.key(computer_choice).to_s
 
-  computer_choice = VALID_CHOICES.sample
+  prompt("You chose: #{player_key} : Computer chose: #{computer_key}")
 
-  prompt("You chose: #{choice}; Computer chose: #{computer_choice}")
-
-  display_results(choice, computer_choice)
-  update_score(score, choice, computer_choice)
+  display_results(player_key, computer_key)
+  update_score(score, player_key, computer_key)
 
   prompt("Your score: #{score[:player]} : Computer score: #{score[:computer]}")
+
   if score[:player] == WINNING_SCORE
     prompt("You win!")
     break
