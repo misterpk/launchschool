126c39e2306df97bb4fb8fec22497bf3201bf29b
nickkim79 <nick.kim@productplan.com>
2019-12-10 18:02:22 -0800

    checking in the first 3 TTT Bonus Features

--- lesson_6/tictactoe.rb
+++ lesson_6/tictactoe.rb
@@ -1,3 +1,5 @@
+require 'byebug'
+
 INITIAL_MARKER = ' '
 PLAYER_MARKER = 'X'
 COMPUTER_MARKER = 'O'
@@ -9,15 +11,29 @@ WINNING_LINES = [
   [1, 5, 9],
   [3, 5, 7]
 ]
+WINNING_SCORE = 5
 
 def prompt(msg)
   puts "=> #{msg}"
 end
 
+def joinor(array, separator=', ', connector='or')
+  case array.size
+  when 0 then ''
+  when 1 then array.first
+  when 2 then array.join(" #{connector} ")
+  else
+    left_string = array[0..-2].join(separator)
+    right_string = "#{separator}#{connector} #{array[-1]}"
+    left_string + right_string
+  end
+end
+
 # rubocop:disable Metrics/AbcSize
-def display_board(board)
+def display_board(board, scores)
   system 'clear'
   puts "You're a #{PLAYER_MARKER}. Computer is an #{COMPUTER_MARKER}"
+  display_score(scores)
   puts ""
   puts "     |     |"
   puts "  #{board[1]}  |  #{board[2]}  |  #{board[3]}"
@@ -47,7 +63,7 @@ end
 def player_places_piece!(board)
   square = ''
   loop do
-    prompt "Choose a square (#{empty_squares(board).join(', ')})"
+    prompt "Choose a square (#{joinor(empty_squares(board))})"
     square = gets.chomp.to_i
     break if empty_squares(board).include?(square)
     prompt "Sorry, that's not a valid choice."
@@ -56,6 +72,14 @@ def player_places_piece!(board)
 end
 
 def computer_places_piece!(board)
+  WINNING_LINES.each do |line|
+    if board.values_at(*line).count(PLAYER_MARKER) == 2
+      line.each do |square|
+        board[square] = COMPUTER_MARKER if board[square] == ' '
+        return
+      end
+    end
+  end
   square = empty_squares(board).sample
   board[square] = COMPUTER_MARKER
 end
@@ -88,10 +112,24 @@ def detect_winner(board)
   nil
 end
 
+def initialize_scores
+  {
+    "Player" => 0,
+    "Computer" => 0
+  }
+end
+
+def display_score(scores)
+  puts "Your Score: #{scores["Player"]}, Computer Score: #{scores["Computer"]}"
+end
+
+scores = initialize_scores
+
 loop do
   board = initialize_board
+
   loop do
-    display_board(board)
+    display_board(board, scores)
     player_places_piece!(board)
     break if someone_won?(board) || board_full?(board)
 
@@ -100,9 +138,17 @@ loop do
   end
 
   if someone_won?(board)
-    prompt "#{detect_winner(board)} won!"
+    winner = detect_winner(board)
+    scores[winner] += 1
+    display_board(board, scores)
+    prompt "#{winner} won!"
   else
     prompt "It's a tie!"
+  end
+
+  if scores[winner] == WINNING_SCORE
+    prompt "#{winner} has scored #{WINNING_SCORE}! Game Over!"
+    break
   end
 
   prompt "Play again? (y or n)"
