1f6facfe52d26aff52304e87ee5b776aee2ca3f5
Nick Kim <nick.kim@gmail.com>
2019-12-23 11:34:32 -0800

    Checking in 2 more fibonacci exercises

---
+++ small_problems/medium_1/fibonacci_numbers_procedural.rb
@@ -0,0 +1,14 @@
+require 'byebug'
+
+def fibonacci(nth)
+  first, last = [1, 1]
+  3.upto(nth) do
+    first, last = [last, first + last]
+  end
+
+  last
+end
+
+puts fibonacci(20) == 6765
+puts fibonacci(100) == 354224848179261915075
+puts fibonacci(100_001) # => 4202692702.....8285979669707537501
