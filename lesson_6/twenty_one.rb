094485a26867299d5c5e92c8e0dccc89faf7a8d4
Nick Kim <nick.kim@gmail.com>
2019-12-25 22:27:34 -0800

    tictactoe refactors

--- lesson_6/twenty_one.rb
+++ lesson_6/twenty_one.rb
@@ -1,8 +1,12 @@
 require 'byebug'
 
-CARDS = %w(2 3 4 5 6 7 8 9 10 Jack Queen King Ace)
-VALUES = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
-SUITS = %w(Hearts Diamonds Clubs Spades)
+CARDS = %w(2 3 4 5 6 7 8 9 10 Jack Queen King Ace).freeze
+VALUES = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11].freeze
+SUITS = %w(Hearts Diamonds Clubs Spades).freeze
+
+def prompt(message)
+  puts "=> #{message}"
+end
 
 def initialize_deck
   cards = []
@@ -38,22 +42,20 @@ def calculate_total(hand)
     number_of_aces.times do
       total <= 21 ? total : total -= 10
     end
-    total
-  else
-    total
   end
+  total
 end
 
 def card_values_string(hand)
   card_string = ""
   if hand.size == 2
-    "#{hand[0][:name]} and #{hand[1][:name]}"
+    "#{card_name(hand[0])} and #{card_name(hand[1])}"
   else
     hand.each_index do |index|
       if index == hand.size - 1
-        return card_string += "and #{hand[index][:name]}"
+        return card_string += "and #{card_name(hand[index])}"
       else
-        card_string += "#{hand[index][:name]}, "
+        card_string += "#{card_name(hand[index])}, "
       end
     end
   end
@@ -63,44 +65,43 @@ def determine_winner(player_hand, dealer_hand)
   dealer_total = calculate_total(dealer_hand)
   player_total = calculate_total(player_hand)
 
-  case
-  when dealer_total > player_total
-    "dealer"
-  when player_total > dealer_total
-    "player"
+  if dealer_total > player_total
+    :dealer
+  elsif player_total > dealer_total
+    :player
   else
-    "tie"
+    :tie
   end
 end
 
 def display_winner(player_hand, dealer_hand)
-  puts "The dealer has #{calculate_total(dealer_hand)}. You have #{calculate_total(player_hand)}"
+  prompt "The dealer has #{calculate_total(dealer_hand)}. " \
+         "You have #{calculate_total(player_hand)}"
   case determine_winner(player_hand, dealer_hand)
-  when "dealer"
-    puts "The dealer wins!"
-  when "player"
-    puts "You win!"
-  when "tie"
-    puts "It's a tie!"
+  when :dealer
+    prompt "The dealer wins!"
+  when :player
+    prompt "You win!"
+  when :tie
+    prompt "It's a tie!"
   end
 end
 
-def bust?(total)
-  total > 21
+def bust?(hand)
+  calculate_total(hand) > 21
 end
 
 def player_turn(deck, hand)
   action = ""
-  player_total = calculate_total(hand)
 
-  until action == "stay" || bust?(player_total)
-    puts "Would you like to hit or stay?"
+  until action == "stay" || bust?(hand)
+    prompt "Would you like to hit or stay?"
     action = gets.chomp
 
     if action == "hit"
       deal_card!(deck, hand)
-      puts "You have: " + card_values_string(hand)
-      player_total = calculate_total(hand)
+      prompt "You have: " + card_values_string(hand)
+      prompt "Your total is #{calculate_total(hand)}"
     end
   end
 end
@@ -112,13 +113,18 @@ def dealer_turn(deck, hand)
     dealer_total = calculate_total(hand)
   end
 
-  puts "Dealer has: #{card_values_string(hand)}"
+  prompt "Dealer has: #{card_values_string(hand)}"
+  prompt "Their total is #{calculate_total(hand)}"
 end
 
 def play_again?
-  puts "Would you like to play again? (Y/n)"
+  prompt "Would you like to play again? (Y/n)"
   play_again = gets.chomp
   play_again.upcase == "Y" || play_again.upcase == "YES"
+end
+
+def card_name(card)
+  "#{card[:name]} of #{card[:suit]}"
 end
 
 loop do
@@ -127,20 +133,20 @@ loop do
   deck = initialize_deck
   deal_initial_hand(deck, player_hand, dealer_hand)
 
-  puts "Dealer has: #{dealer_hand[0][:name]} and an unknown card"
-  puts "You have: " + card_values_string(player_hand)
-
+  prompt "Dealer has: #{card_name(dealer_hand[0])} and an unknown card"
+  prompt "You have: " + card_values_string(player_hand) +
+         ", for a total of #{calculate_total(player_hand)}"
   player_turn(deck, player_hand)
 
-  if bust?(calculate_total(player_hand))
-    puts "You busted! Dealer wins!"
+  if bust?(player_hand)
+    prompt "You busted! Dealer wins!"
     play_again? ? next : break
   end
 
   dealer_turn(deck, dealer_hand)
 
-  if bust?(calculate_total(dealer_hand))
-    puts "The dealer busted! You win!"
+  if bust?(dealer_hand)
+    prompt "The dealer busted! You win!"
     play_again? ? next : break
   end
 
@@ -148,4 +154,4 @@ loop do
   play_again? ? next : break
 end
 
-puts "Thanks for playing Twenty One!"
+prompt "Thanks for playing Twenty One!"
