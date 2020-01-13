f2d3ccb8a557024daa914bd2974d889f7958aafc
Nick Kim <nick.kim@gmail.com>
2020-01-12 23:19:50 -0800

    finishing further exploration for rotate90

--- small_problems/advanced_1/rotating_matrices.rb
+++ small_problems/advanced_1/rotating_matrices.rb
@@ -16,6 +16,27 @@ def rotate90(matrix)
   result
 end
 
+def ls_rotate90(matrix)
+  result = []
+  number_of_rows = matrix.size
+  number_of_columns = matrix.first.size
+  (0...number_of_columns).each do |column_index|
+    new_row = (0...number_of_rows).map do |row_index|
+      matrix[row_index][column_index]
+    end
+    result << new_row.reverse
+  end
+  result
+end
+
+def rotate(matrix, degrees)
+  new_matrix = matrix.clone
+  (degrees / 90).times do
+    new_matrix = rotate90(new_matrix)
+  end
+  new_matrix
+end
+
 matrix1 = [
   [1, 5, 8],
   [4, 7, 2],
@@ -30,7 +51,9 @@ matrix2 = [
 new_matrix1 = rotate90(matrix1)
 new_matrix2 = rotate90(matrix2)
 new_matrix3 = rotate90(rotate90(rotate90(rotate90(matrix2))))
+new_matrix4 = rotate(matrix2, 360)
 
 p new_matrix1 == [[3, 4, 1], [9, 7, 5], [6, 2, 8]]
 p new_matrix2 == [[5, 3], [1, 7], [0, 4], [8, 2]]
 p new_matrix3 == matrix2
+p new_matrix4 == matrix2
