72c0e6708aeb1282e24399a06611da0e2a2d23de
misterpk <nick.kim@gmail.com>
2020-01-19 10:30:22 -0800

    adding LS solution (much better than mine)

--- small_problems/advanced_1/merge_sorted_lists.rb
+++ small_problems/advanced_1/merge_sorted_lists.rb
@@ -1,32 +1,48 @@
 require 'byebug'
 
 def merge(array1, array2)
-  if array1.empty?
-    return array2
-  elsif array2.empty?
-    return array1
-  end
+  # key point is that you need to keep track of items in the second array while
+  # doing comparisons, my way works, but the logic is convoluted the LS solution
+  # is much more elegant and probably more efficient
+  # LS solution
+  index2 = 0
+  result = []
 
-  new_sorted_array = []
-  array2_index = 0
-
-  array1.each_index do |index|
-    if array2[array2_index].nil?
-      new_sorted_array.push(array1.slice(array1[index...array1.size]))
-    elsif array1[index].nil?
-      new_sorted_array.push(array2.slice(array2[index...array2.size]))
-    elsif array1[index] < array2[array2_index]
-      new_sorted_array.push(array1[index])
-    elsif array1[index] > array2[array2_index]
-      until array1[index] < array2[array2_index]
-        new_sorted_array.push(array2[array2_index])
-        array2_index += 1
-        break if array2[array2_index].nil?
-      end
-      new_sorted_array.push(array1[index])
+  array1.each do |value|
+    while index2 < array2.size && array2[index2] <= value
+      result << array2[index2]
+      index2 += 1
     end
+    result << value
   end
-  new_sorted_array
+
+  result.concat(array2[index2..-1])
+  #if array1.empty?
+  #  return array2
+  #elsif array2.empty?
+  #  return array1
+  #end
+  #
+  #new_sorted_array = []
+  #array2_index = 0
+  #
+  #array1.each_index do |index|
+  #  if array2[array2_index].nil?
+  #    new_sorted_array.push(array1.slice(array1[index...array1.size]))
+  #  elsif array1[index].nil?
+  #    new_sorted_array.push(array2.slice(array2[index...array2.size]))
+  #  elsif array1[index] < array2[array2_index]
+  #    new_sorted_array.push(array1[index])
+  #  elsif array1[index] > array2[array2_index]
+  #    until array1[index] < array2[array2_index]
+  #      new_sorted_array.push(array2[array2_index])
+  #      array2_index += 1
+  #      break if array2[array2_index].nil?
+  #    end
+  #    new_sorted_array.push(array1[index])
+  #  end
+  #end
+  #new_sorted_array
 end
 
 p merge([1, 5, 9], [2, 6, 8]) == [1, 2, 5, 6, 8, 9]
