30065ea237209841ef5b3eb9ea9d850df97a1c98
misterpk <nick.kim@gmail.com>
2020-01-22 13:59:25 -0800

    checking in 4 and 5 for debugging

--- small_problems/debugging/valid_series.rb
+++ small_problems/debugging/valid_series.rb
@@ -3,7 +3,7 @@ require 'byebug'
 def valid_series?(nums)
   return false if nums.sum != 47
 
-  odd_count = nums.count { |n| n.odd? }
+  odd_count = nums.count(&:odd?)
   odd_count == 3 ? true : false
 end
 
